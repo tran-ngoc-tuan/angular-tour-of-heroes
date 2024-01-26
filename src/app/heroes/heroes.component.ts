cc37a3ce7ff857acc3575f8eaab46eddf57ac2ab
Trần Ngọc Tuấn (KHPC-VTCNTT.CV) <tuantn3@cpc.vn>
2024-01-26 09:44:22 +0700

    fourn  commit Tour of herroes- thay doi

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss'
 })
 export class HeroesComponent implements OnInit {
   public hero = 'Windstorm';
   public hero1: Hero= {id:1, name: 'Windstorm'}
+test: any;
   ngOnInit(): void {
   }
 
